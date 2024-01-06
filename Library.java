59062e927ba22459fae7f9179e244e239288a935
DeepaMaskuri <143724723+DeepaMaskuri@users.noreply.github.com>
2024-01-06 17:26:26 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,9 @@
+package day4;
+
+public interface Library {
+void addBook(Book b);
+ void displayBookDetails(String a);
+ void removeBook(int id );
+ void updateBookDetails(int id);
+ void bookSummary();
+}
